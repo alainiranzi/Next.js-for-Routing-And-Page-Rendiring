9e4b77f114f507e9f60d4050163d36e5ce4eb6c1
Alain Iranzi <alainiranzi00@gmail.com>
2026-02-19 12:22:57 +0200

    styles links in news using globals.css

--- app/news/page.js
+++ app/news/page.js
@@ -1,10 +1,11 @@
+import '../globals.css';
 import Link from "next/link";
 
 export default function NewsPage() {
 return (
 <>
 <h1>News Page</h1>
-<ul>
+<ul className="news-list">
     <li>
         <Link href="/news/first-news">First News Item</Link>
     </li>
