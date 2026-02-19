b247bd46a1cc92cb0a4c71fa75e1199ee6f06f9e
Alain Iranzi <alainiranzi00@gmail.com>
2026-02-19 12:16:00 +0200

    styled through layout

--- app/layout.js
+++ app/layout.js
@@ -4,15 +4,17 @@ import './globals.css';
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
-}
+};
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
       <body>
-        <MainHeader />
-        {children}
-        </body>
+        <div id="page">
+          <MainHeader />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
