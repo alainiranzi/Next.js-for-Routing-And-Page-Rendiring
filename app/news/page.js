a1e95854f67ed47c4cf8227113d41fbf5f9df363
Alain Iranzi <alainiranzi00@gmail.com>
2026-02-20 10:45:46 +0200

    adding  dummy data

--- app/news/page.js
+++ app/news/page.js
@@ -1,20 +1,19 @@
 import '../globals.css';
 import Link from "next/link";
+import { DUMMY_NEWS } from '@/dummy-news';
 
 export default function NewsPage() {
 return (
 <>
 <h1>News Page</h1>
 <ul className="news-list">
-    <li>
-        <Link href="/news/first-news">First News Item</Link>
-    </li>
-    <li>
-        <Link href="/news/second-news">Second News Item</Link>
-    </li>
-    <li>
-        <Link href="/news/third-news"> Third News Item</Link> 
-    </li>
+    {DUMMY_NEWS.map((news) => (
+        <li key={news.id}>
+            <Link href={`/news/${news.slug}`}>{news.title}
+            <img src={`/images/news/${news.image}`} />
+            </Link>
+        </li>
+    ))}
 </ul>
 
 </>
