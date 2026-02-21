771ad8b2e69cba704e4edabc6af03748c98cf26d
Alain Iranzi <alainiranzi00@gmail.com>
2026-02-21 13:42:11 +0200

    added a not found file

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+    return (
+        <div id="error">
+            <h1>Nt Found</h1>
+            <p>The Requested resource could not be found!</p>
+        </div>
+    );
+}
