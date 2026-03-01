0001769f7fe44b1d47c51eb0d503b2a7108186ca
Alain Iranzi <alainiranzi00@gmail.com>
2026-03-01 14:04:30 +0200

    dealing with page.js of [year]

--- app/archive/@archive/[year]/page.js
+++ app/archive/@archive/[year]/page.js
@@ -1,3 +1,6 @@
+import NewsList from "@/components/NewsList";
+import { getNewsForYear } from "@/lib/news";
+
 export default function FilteredNewsPage({ params }) {
     const newsYear = params.year;
     const news = getNewsForYear(newsYear);
