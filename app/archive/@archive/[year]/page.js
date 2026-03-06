c4d56366a1d1e6aba981cb6c8a61d4c98c8e192c
Alain Iranzi <alainiranzi00@gmail.com>
2026-03-06 18:37:05 +0200

    parraller and nested routes are working now

--- app/archive/@archive/[year]/page.js
+++ app/archive/@archive/[year]/page.js
@@ -1,10 +1,12 @@
-// app/archive/@archive/[year]/page.js
 import NewsList from '@/components/news-list';
 import { getNewsForYear } from '@/lib/news';
 
-export default function FilteredNewsPage({ params }) {
-  const newsYear = params.year;
-  const news = getNewsForYear(newsYear);
+export default async function FilteredNewsPage({ params }) {
+  
+  const { year } = await params;
+
+  const news = getNewsForYear(year);
+  console.log('Year:', year, 'News found:', news);
 
   return <NewsList news={news} />;
 }
