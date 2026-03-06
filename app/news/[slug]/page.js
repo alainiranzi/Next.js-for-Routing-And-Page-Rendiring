dd13b5a11cd3de44bcbb572c077c0cdc973282cf
Alain Iranzi <alainiranzi00@gmail.com>
2026-03-06 15:52:27 +0200

    fixingthe connections of news with dummy

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,28 +1,24 @@
-import { notFound } from "next/navigation";
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
 
 export default function NewsDetailPage({ params }) {
-  const newsSlug = params.slug;
+  console.log("PARAMS:", params);
 
   const newsItem = DUMMY_NEWS.find(
-    (newsItem) => newsItem.slug === newsSlug
+    (newsItem) => newsItem.slug === params.slug
   );
 
   if (!newsItem) {
+    console.log("News not found!");
     notFound();
   }
 
   return (
-    <article className="news-article">
+    <article>
       <header>
-        <img
-          src={`/images/news/${newsItem.image}`}
-          alt={newsItem.title}
-        />
+        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
         <h1>{newsItem.title}</h1>
-        <time dateTime={newsItem.date}>
-          {newsItem.date}
-        </time>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
       <p>{newsItem.content}</p>
     </article>
