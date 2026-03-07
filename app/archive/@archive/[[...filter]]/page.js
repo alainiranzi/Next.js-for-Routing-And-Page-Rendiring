96ab27a854675cd20a7131ef6181c022b25ff6e8
Alain Iranzi <alainiranzi00@gmail.com>
2026-03-07 17:19:57 +0200

    added months in filters

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,41 +1,47 @@
 import Link from 'next/link';
 import NewsList from '@/components/news-list';
-import { getAvailableNewsYears, getNewsForYear } from '@/lib/news';
+import { getAvailableNewsYears, getAvailableNewsMonths, getNewsForYear } from '@/lib/news';
 
 export default async function FilteredNewsPage({ params }) {
-  // await kuri params nk'uko bikenewe
   const { filter } = await params;
 
-  const selectedYear = filter?.[0]; // year iri muri index 0
-  const selectedMonth = filter?.[1]; // niba month iriho
+  const selectedYear = filter?.[0]; 
+  const selectedMonth = filter?.[1]; 
 
   let news;
+  let links = getAvailableNewsYears();
 
-  // niba ufite year gusa cyangwa na month
+
   if (selectedYear) {
     news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
   }
 
-  // umwanya wo kwerekana content
+  
   let newsContent = <p>No news found for the selected period.</p>;
 
-  // niba news yabonetse, yohereze muri NewsList
+  
   if (news && news.length > 0) {
     newsContent = <NewsList news={news} />;
   }
 
-  const links = getAvailableNewsYears();
 
   return (
     <>
       <header id="archive-header">
         <nav>
           <ul>
-            {links.map((link) => (
+            {links.map((link) => {
+              const href = selectedYear
+               ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
               <li key={link}>
-                <Link href={`/archive/${link}`}>{link}</Link>
+                <Link href={href}>{link}</Link>
               </li>
-            ))}
+            );
+            })}
           </ul>
         </nav>
       </header>
